3ea34662ec253ff21b18ead6e60d195280b212a0
lewis-revill <lewis.revill@embecosm.com>
2019-05-30 10:21:54 +0100

    [RISCV] Add support for save/restore of CSRs via libcalls
    This patch adds the support required for using the __riscv_save and
    __riscv_restore libcalls to implement a size-optimization for prologue
    and epilogue code, whereby the spill and restore code of callee-saved
    registers is implemented by common functions to reduce code duplication.

---
+++ lib/Target/RISCV/RISCVMachineFunctionInfo.cpp
@@ -0,0 +1,34 @@
+//=- RISCVMachineFunctionInfo.cpp - RISCV machine function info ---*- C++ -*-=//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+// This file defines functionality for RISCVMachineFunctionInfo.
+//
+//===----------------------------------------------------------------------===//
+
+#include "RISCVMachineFunctionInfo.h"
+
+using namespace llvm;
+
+static cl::opt<bool> EnableSaveRestore("enable-save-restore", cl::init(false),
+    cl::desc("Enable save/restore of callee-saved registers via libcalls"));
+
+bool RISCVMachineFunctionInfo::useSaveRestoreLibCalls() const {
+  const MachineFrameInfo &MFI = MF.getFrameInfo();
+
+  if (!EnableSaveRestore)
+    return false;
+
+  // We cannot use fixed locations for the callee saved spill slots if there is
+  // any chance of other objects also requiring fixed locations in the stack
+  // frame. This is called before adding the fixed spill slots so there should
+  // be no fixed objects at all.
+  if (MFI.getNumFixedObjects())
+    return false;
+
+  return true;
+}
